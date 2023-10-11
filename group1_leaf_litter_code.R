c35f6a2cb483f0ad6f985b4245d12b987f212967
Tyler <tchristianbk@gmial.com>
2023-10-11 12:53:15 +0100

    initial commit

---
+++ group1_leaf_litter_code.R
@@ -0,0 +1,33 @@
+
+# To do -------------------------------------------------------------------
+
+# Calculate alpha diversity
+# GLM for alpha diversity V Section
+# Boxplot for diversity per section
+# Histogram for orders found (stacked per section?)
+
+# Install packages --------------------------------------------------------
+
+#install.packages('tidyverse')
+library(tidyverse)
+#install.packages('patchwork')
+library(patchwork)
+
+?cor.test
+
+# Install data ------------------------------------------------------------
+
+
+# Alpha diversity ---------------------------------------------------------
+
+
+# GLM ---------------------------------------------------------------------
+
+
+# Boxplot -----------------------------------------------------------------
+
+
+# Histogram ---------------------------------------------------------------
+
+
+

1785167ef7ba730e6e3e1bba961db8e87af78aad
Tyler <tchristianbk@gmial.com>
2023-10-11 15:21:01 +0100

    finished diversity +plot

--- group1_leaf_litter_code.R
+++ group1_leaf_litter_code.R
@@ -1,4 +1,5 @@
 
+
 # To do -------------------------------------------------------------------
 
 # Calculate alpha diversity
@@ -15,39 +16,70 @@ library(patchwork)
 #install.packages('vegan')
 library(vegan)
 
-?cor.test
+? cor.test
 
 # Install data ------------------------------------------------------------
 
-insect_data <- read.csv('group1_leaf_litter_data.csv') %>% 
-  mutate(wood_name = as.factor(wood_name)) 
-            
+insect_data <- read.csv('group1_leaf_litter_data.csv') %>%
+  mutate(wood_name = as.factor(wood_name)) %>%
+  mutate(transect_point = as.factor(transect_point)) %>%
+  select(transect_point, cockroaches.woodlice:earwig) %>%
+  pivot_longer(cols = cockroaches.woodlice:earwig,
+               names_to = "species",
+               values_to = "amount") %>%
+  pivot_wider(names_from = transect_point,
+              values_from = amount)
+
 View(insect_data)
 
 str(insect_data)
 
 alpha_data <-
-  insect_data %>% 
-  select(transect_point, cockroaches.woodlice:earwig) %>% 
-  pivot_longer(cols = cockroaches.woodlice:earwig,
-               names_to = "species",
-               values_to = "amount") %>% 
-  pivot_wider(names_from = transect_point,
-              values_from = amount)
+  read.csv('group1_leaf_litter_alpha_diversity_data.csv', header = TRUE)
 
 View(alpha_data)
 
 # Alpha diversity ---------------------------------------------------------
 
-simpson.unb(x = insect_data[3], "simpson")
+# Grassland Vs Ecotone
+cor.test(alpha_data$grassland, alpha_data$ecotone, method = "spearman")
+
+# Woodland Vs Ecotone
+cor.test(alpha_data$woodland, alpha_data$ecotone, method = "spearman")
+
+# Woodland Vs Grassland
+cor.test(alpha_data$woodland, alpha_data$grassland, method = "spearman")
 
 # GLM ---------------------------------------------------------------------
-cor.test()
 
-# Boxplot -----------------------------------------------------------------
+combination <-
+  c("Grassland + Ecotone",
+    "Woodland + Ecotone",
+    "Grassland + Woodland")
+rho <- c(0.106483, 0.7649942, 0.244653)
 
+glm_matrix <- data.frame(combination, rho)
+
+View(glm_matrix)
+
+# Rho plot ----------------------------------------------------------------
+
+glm_matrix %>%
+  ggplot(aes(
+    x = combination,
+    y = rho,
+    label = rho,
+    fill = combination
+  )) +
+  geom_col() +
+  geom_text(vjust = -0.5) +
+  theme(
+    axis.title.x = element_blank(),
+    panel.background = element_blank(),
+    panel.grid.major = element_line(colour = "lightgrey"),
+    axis.line = element_line(colour = "black"),
+    legend.position = "none"
+  )
 
 # Histogram ---------------------------------------------------------------
-
-
 

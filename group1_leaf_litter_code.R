7de71476034c1bcff3c4d3be56560aab44451793
Tyler <tchristianbk@gmial.com>
2023-10-11 14:06:51 +0100

    starting alpha diversity

--- group1_leaf_litter_code.R
+++ group1_leaf_litter_code.R
@@ -12,17 +12,37 @@
 library(tidyverse)
 #install.packages('patchwork')
 library(patchwork)
+#install.packages('vegan')
+library(vegan)
 
 ?cor.test
 
 # Install data ------------------------------------------------------------
 
+insect_data <- read.csv('group1_leaf_litter_data.csv') %>% 
+  mutate(wood_name = as.factor(wood_name)) 
+            
+View(insect_data)
+
+str(insect_data)
+
+alpha_data <-
+  insect_data %>% 
+  select(transect_point, cockroaches.woodlice:earwig) %>% 
+  pivot_longer(cols = cockroaches.woodlice:earwig,
+               names_to = "species",
+               values_to = "amount") %>% 
+  pivot_wider(names_from = transect_point,
+              values_from = amount)
+
+View(alpha_data)
 
 # Alpha diversity ---------------------------------------------------------
 
+simpson.unb(x = insect_data[3], "simpson")
 
 # GLM ---------------------------------------------------------------------
-
+cor.test()
 
 # Boxplot -----------------------------------------------------------------
 

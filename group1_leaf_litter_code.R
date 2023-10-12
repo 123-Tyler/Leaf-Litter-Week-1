ec790bbd850dfb2da1e12e84cede2b0a2752d998
Tyler <tchristianbk@gmial.com>
2023-10-12 16:17:14 +0100

    Final commit

--- group1_leaf_litter_code.R
+++ group1_leaf_litter_code.R
@@ -12,10 +12,17 @@
 library(tidyverse)
 #install.packages('vegan')
 library(vegan)
+#install.packages('patchwork')
+library(patchwork)
 
 # Install data ------------------------------------------------------------
+whole_data <- 
+  read.csv('group1_leaf_litter_data_new.csv')
 
-insect_data <- read.csv('group1_leaf_litter_data_new.csv') %>%
+View(whole_data)
+
+insect_data <- 
+  read.csv('group1_leaf_litter_data_new.csv') %>%
   select(1:26) %>%
   mutate(wood_name = as.factor(wood_name)) %>%
   mutate(transect_point = as.factor(transect_point)) %>%
@@ -49,6 +56,9 @@ cor.test(alpha_data$grassland, alpha_data$ecotone, method = "spearman")
 # Woodland Vs Ecotone
 cor.test(alpha_data$woodland, alpha_data$ecotone, method = "spearman")
 
+cor.test(alpha_data$woodland, method = "shannon") # look at this
+
+
 # Woodland Vs Grassland
 cor.test(alpha_data$woodland, alpha_data$grassland, method = "spearman")
 
@@ -59,25 +69,27 @@ combination <-
     "Woodland + Ecotone",
     "Grassland + Woodland")
 rho <- c(0.07971706, 0.764959, 0.2179379)
+p <- c("p = 0.75", "p < 0.01", "p = 0.39")
 
-glm_matrix <- data.frame(combination, rho)
+alpha_matrix <- data.frame(combination, rho, p)
 
-View(glm_matrix)
+View(alpha_matrix)
 
 # Rho plot ----------------------------------------------------------------
 
-glm_matrix %>%
+alpha_matrix %>%
   ggplot(aes(
     x = combination,
     y = rho,
-    label = rho,
+    label = p,
     fill = combination
   )) +
   geom_col() +
   ggtitle("Alpha Diversity Using Spearman's Rank") +
+  ylab("Alpha Diversity (rho)") +
   geom_text(vjust = -0.5, 
             size = 5) +
-  scale_fill_manual(values = c("darkorange2", "#00AEDA", "#00B159")) +
+  scale_fill_manual(values = c("blue2", "yellow2", "grey")) +
   theme(
     axis.title.x = element_blank(),
     panel.background = element_blank(),
@@ -96,14 +108,14 @@ ggsave("alpha_diversity_hist.jpeg",
        height = 9,
        units = "in")
 
-# Histogram ---------------------------------------------------------------
+# Invertebrate abundance ---------------------------------------------------------------
 
-species_data %>%
+p1 <- species_data %>%
   pivot_longer(cols = Grassland:Woodland,
                names_to = "Habitat",
                values_to = "Frequency") %>%
   ggplot(aes(
-    x = reorder(str_to_title(species), Frequency),
+    x = reorder(str_to_title(species), -Frequency),
     y = Frequency,
     fill = Habitat
   )) +
@@ -111,7 +123,7 @@ species_data %>%
   coord_flip() +
   ylab("Total Frequency") +
   xlab("Order") +
-  ggtitle("Invertabrate Diversity Across the Transects") +
+  ggtitle("Invertabrate Abundance Across the Transects") +
   scale_fill_manual(values = c("darkorange2", "#00AEDA", "#00B159")) +
   theme(
     panel.background = element_blank(),
@@ -127,7 +139,44 @@ species_data %>%
                               face = "bold")
   )
 
-ggsave("species_hist.jpeg",
+# Total Species -----------------------------------------------------------
+
+Habitat <- c("Ecotone", "Grassland", "Woodland")
+Count <- c(12, 10, 14)
+total_inverts <- c(75, 60, 112)
+
+total_species <- data.frame(Habitat, Count, total_inverts)
+
+View(total_species)
+
+p2 <- total_species %>% 
+  ggplot(aes(
+    x = Habitat,
+    y = Count,
+  )) +
+  geom_col(fill = c("darkorange2", "#00AEDA", "#00B159")) +
+  ylab("Total Orders Sampled") +
+  theme(
+    panel.background = element_blank(),
+    axis.line = element_line(colour = "black"),
+    axis.text = element_text(size = 13),
+    axis.title.y = element_text(size = 15,
+                              face = "bold"),
+    axis.title.x = element_blank(),
+    legend.text = element_text(size = 13),
+    legend.title = element_text(size = 15, 
+                                face = "bold"),
+    plot.title = element_text(size = 17,
+                              face = "bold")
+  )
+
+# Combining plots ---------------------------------------------------------
+
+p1 + inset_element(p2, 0.6, 0.6, 1, 1)
+
+ggsave("insect_abundance_plot.jpeg",
        width = 16,
        height = 9,
        units = "in")
+
+t.test(total_species$total_inverts)
